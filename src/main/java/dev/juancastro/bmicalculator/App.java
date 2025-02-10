99750f6db8ae274a4e105a38dcf51113161ecbeb
juancastro000 <cdyutub@gmail.com>
2025-02-10 03:02:58 +0100

    app test pass

--- src/main/java/dev/juancastro/bmicalculator/App.java
+++ src/main/java/dev/juancastro/bmicalculator/App.java
@@ -1,3 +1,12 @@
 package dev.juancastro.bmicalculator;
 
 
+public class App {
+    public static void main(String[] args) {
+        Persona persona = new Persona(70, 1.75);
+        BmiCalculator calculator = new BmiCalculator();
+        
+        double bmi = calculator.calculateBMI(persona);
+        System.out.println("El IMC es: " + bmi);
+    }
+}
